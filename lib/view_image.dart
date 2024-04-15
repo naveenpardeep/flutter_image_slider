eaa461d5155c29c18cdd482d7e553839937167df
naveenpardeep <naveensheoran1000@gmail.com>
2024-04-15 14:58:29 +0300

    readme

---
+++ lib/view_image.dart
@@ -0,0 +1,103 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+class CardItemView extends StatelessWidget {
+  const CardItemView({super.key, required this.items});
+  final List<String> items;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          //imagePArt here
+          ImageSliderView(
+            imagesPath: items,
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
+  }
+}
+
+class ImageSliderView extends StatefulWidget {
+  const ImageSliderView({
+    super.key,
+    this.imageHeight = 200.0,
+    required this.imagesPath,
+  });
+  //get imageURLs
+  final double imageHeight; // or use aspect ratio
+  final List<String> imagesPath;
+
+  @override
+  State<ImageSliderView> createState() => _ImageSliderViewState();
+}
+
+class _ImageSliderViewState extends State<ImageSliderView> {
+  int _current = 0;
+  final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      setState(() {
+        _current = _pageController.page?.round() ?? 0;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final counterView = Row(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        for (int i = 0; i < widget.imagesPath.length; i++)
+          Container(
+            width: 8.0,
+            height: 8.0,
+            margin: const EdgeInsets.symmetric(horizontal: 4.0),
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: _current == i ? Colors.blue : Colors.grey,
+            ),
+          ),
+      ],
+    );
+    return SizedBox(
+      height: widget.imageHeight,
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: PageView.builder(
+                itemCount: widget.imagesPath.length,
+                controller: _pageController,
+                itemBuilder: (context, index) => ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Image.network(
+                        widget.imagesPath[index],
+                        fit: BoxFit.fill,
+                      ),
+                    )),
+          ),
+          Positioned(
+            bottom: 8,
+            left: 0,
+            right: 0,
+            child: counterView,
+          )
+        ],
+      ),
+    );
+  }
+}
