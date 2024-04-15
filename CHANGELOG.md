2ae807b5b7e5d34f75cfec75c1daeac7435cb05f
naveenpardeep <naveensheoran1000@gmail.com>
2024-04-15 15:47:56 +0300

    screenshot

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
-## 0.0.1
+## 0.0.2
 
 Cool flutter image carousel slider 
+photos example added
--- lib/flutter_image_slider.dart
+++ lib/flutter_image_slider.dart
@@ -2,3 +2,4 @@ library flutter_image_slider;
 
 export 'list_image_view.dart';
 export 'photo_view_page.dart';
+export 'view_image.dart';
